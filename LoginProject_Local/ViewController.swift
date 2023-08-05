97f7c66935c1b506af2c8c779fb713580abe8ca6
angwoo0503 <angwoo0503@gmail.com>
2023-08-05 19:17:28 +0900

    add

---
+++ LoginProject_Local/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  LoginProject_Local
+//
+//  Created by t2023-m0061 on 2023/08/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let emailTextFieldView : UIView = {
+
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        makeUI()
+        
+    }
+
+    func makeUI() {
+
+    }
+
+}
+
